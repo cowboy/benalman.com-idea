52e287c1a666b4c05bd9b1ce68bd1bfb1725cdca
Ben Alman <cowboy@rj3.net>
2013-02-21 17:55:01 -0500

    TOC / header anchor generation

--- Gruntfile.js
+++ Gruntfile.js
@@ -97,19 +97,31 @@ module.exports = function(grunt) {
 
   var yamljs = require('js-yaml');
 
+  // Add pages-to-be-checked to nav.
+  var pageUrls = [];
+  function id(pageid) {
+    if (pageUrls.indexOf(pageid) === -1) {
+      console.log(pageid);
+      pageUrls.unshift(pageid);
+    }
+  }
+
   grunt.registerMultiTask('massage', 'Massage some stuff.', function() {
     this.files.forEach(function(f, i) {
       var indexPre = grunt.file.read(path.join(f.src[0], 'index.md'));
       var meta = grunt.file.readYAML(path.join(f.src[0], 'meta.yaml'));
-      var extras = {
+      var o = {
         src: f.src,
         dest: f.dest,
         id: path.basename(f.src[0]),
       };
-      meta = massageMetadata(meta, extras);
+      // Exclude any page with a @tag.
+      if (/@/.test(meta.tags.join(' '))) { return; }
+
+      meta = massageMetadata(meta, o);
       var yaml = yamljs.dump(meta, {flowLevel: 1});
       grunt.file.write(path.join(f.dest, 'meta.yaml'), yaml);
-      var indexPost = massageIndex(indexPre, extras);
+      var indexPost = massageIndex(indexPre, o);
       grunt.file.write(path.join(f.dest, 'index.md'), indexPost);
       // logging
       var log = 0;
@@ -142,7 +154,6 @@ module.exports = function(grunt) {
 
     // INLINE SOURCE CODE -> EXTERNAL FILES
     var i = 0;
-    var logged, id = function() { if (!logged) { logged = 1; console.log(o.id); } };
     // Find <pre class="brush:js">...</pre>.
     s = s.replace(/<pre(\b[^>]*)>\n*([\s\S]*?)\n*<\/pre>/gi, function(_, attrs, code) {
       var attrMatches = attrs.match(/brush:([a-z]+)/i);
@@ -168,7 +179,7 @@ module.exports = function(grunt) {
       }
       code += '\n';
       grunt.file.write(path.join(o.dest, filename), code);
-      return '<%= include(\'' + filename + '\') %>';
+      return '<!-- include ' + filename + ' -->';
     });
 
     // Fix indented lines starting with [
@@ -176,39 +187,84 @@ module.exports = function(grunt) {
       return '[';
     });
 
+    // Replace manually-entered TOC with dynamic TOC.
+    var p = [
+      '2012-09-25-partial-application-in-javascript',
+      // '2010-03-28-jquery-special-events',
+    ];
+    if (p.indexOf(o.id) !== -1) {
+      s = s.replace(/(^\s*\*.*$\n)+/m, '\n\n<!-- toc -->\n\n');
+      // id(o.id);
+    }
+
     return s;
   }
 
   var jade = require('jade');
   var hljs = require('highlight.js');
+  var slug = require('slug');
+  var entities = require('entities');
   var marked = require('marked');
   marked.setOptions({});
 
   grunt.registerMultiTask('build', 'Build some stuff.', function() {
-    var pageUrls = this.filesSrc.map(path.basename).reverse();
+    if (!pageUrls || pageUrls.length === 0) {
+      pageUrls = this.filesSrc.map(path.basename).reverse();
+    }
     this.files.forEach(function(f, i) {
-      var md = grunt.file.read(path.join(f.src[0], 'index.md'));
-      var content = grunt.util._.template(md, {
-        include: function(filename) {
-          var src = grunt.file.read(path.join(f.src[0], filename));
-          var lang = path.extname(filename).slice(1);
-          var langMap = {
-            js: 'javascript',
-            html: 'xml',
-          };
-          if (lang in langMap) { lang = langMap[lang]; }
-          var hlsource = /^te?xt$/.test(lang) ? src : hljs.highlight(lang, src).value;
-          return '\n<pre><code>' + hlsource + '</code></pre>';
-        }
+      var s = grunt.file.read(path.join(f.src[0], 'index.md'));
+      var title = s.match(/#\s+(.*)/)[1];
+
+      // Parse as markdown.
+      s = marked(s);
+
+      // Add anchor links to headers and build a TOC.
+      var toc = [];
+      s = s.replace(/(<h(\d)>)([^<]+)(<\/h\2>)/g, function(_, open, depth, header, close) {
+        depth = Number(depth);
+        if (depth === 1) { return _; }
+        var name = header.toLowerCase();
+        name = entities.decode(name);
+        name = name.replace(/'/g, '');
+        name = name.replace(/[:"!\(\)]+/g, '-');
+        name = slug(name);
+        // toc.push({header: header, name: name});
+        toc.push(new Array(depth - 1).join('  ') + '* [' + header + '](#' + name + ')');
+        var anchor = '<a href="#' + name + '" name="' + name + '">' + header + '</a>';
+        return open + anchor + close;
       });
-      var title = content.match(/#\s+(.*)/)[1];
-      content = marked(content);
+      toc = toc.join('\n');
+
+      // Replace <!-- ??? --> comment directives with stuff.
+      s = s.replace(/<!--\s*(.*?)\s*-->/g, function(_, args) {
+        args = args.split(/\s+/);
+        var cmd = args.shift();
+        var map = {
+          toc: function() {
+            return marked(toc);
+          },
+          include: function(filename) {
+            var src = grunt.file.read(path.join(f.src[0], filename));
+            var lang = path.extname(filename).slice(1);
+            var langMap = {
+              js: 'javascript',
+              html: 'xml',
+            };
+            if (lang in langMap) { lang = langMap[lang]; }
+            var hlsource = /^te?xt$/.test(lang) ? src : hljs.highlight(lang, src).value;
+            return '\n<pre><code>' + hlsource + '</code></pre>';
+          },
+        };
+        return cmd in map ? map[cmd].apply(null, args) : _;
+      });
+
+      // Compile output with Jade.
       var output = jade.compile(grunt.file.read('src/jade/page.jade'), {
         // nothing
       })({
         title: title,
         url: path.basename(f.src[0]),
-        content: content,
+        content: s,
         pageUrls: pageUrls,
       });
       grunt.file.write(f.dest, output);
